8dd4b447f835fd15f5e574e5cde06129fc7d0e20
kreativerohail <rohail.codes@gmail.com>
2026-01-22 14:49:39 +0000

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Home from "../sections/Home.jsx" 
+import Services from '../sections/Services.jsx'
 const Main = () => {
   return (
     <div>
         <Home/>
+        <Services/>
     </div>
   )
 }
