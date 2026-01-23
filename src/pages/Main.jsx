07d46044d77579596158985554803b3573d86cd0
kreativerohail <rohail.codes@gmail.com>
2026-01-23 11:24:57 +0000

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Home from "../sections/Home.jsx" 
 import Services from '../sections/Services.jsx'
 import ContactSection from "../sections/ContactSection.jsx"
+import WhyCtrl4 from '../sections/WhyCtrl4.jsx'
 const Main = () => {
   return (
     <div>
         <Home/>
         <Services/>
         <ContactSection/>
+        <WhyCtrl4/>
     </div>
   )
 }
