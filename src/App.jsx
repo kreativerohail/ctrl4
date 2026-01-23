7ea2f95a676075a2abec81892973f254ba83c732
kreativerohail <rohail.codes@gmail.com>
2026-01-23 12:05:10 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SolutionConsulting from "./pages/SolutionConsulting.jsx";
 import BPOAdvisory from "./pages/BPOAdvisory.jsx";
 import CybersecurityAwareness from "./pages/CybersecurityAwareness.jsx";
 import SmeGrowthEnablement from "./pages/SmeGrowthEnablement.jsx";
+import About from "./pages/About.jsx";
 
 export default function App() {
   return (
@@ -28,6 +29,7 @@ export default function App() {
           <Route path="/bpo-advisory" element={<BPOAdvisory/>} />
           <Route path="/cybersecurity-awareness" element={<CybersecurityAwareness/>} />
           <Route path="/smegrowth-enablement" element={<SmeGrowthEnablement/>} />
+          <Route path="/about" element={<About/>} />
         </Routes>
         <PremiumFooter/>
       </div>
