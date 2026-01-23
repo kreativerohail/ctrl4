d6085861dd95dba803d168f0b447aeef77bb4abf
kreativerohail <rohail.codes@gmail.com>
2026-01-23 12:42:01 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ export default function App() {
           <Route path="/cybersecurity-awareness" element={<CybersecurityAwareness/>} />
           <Route path="/smegrowth-enablement" element={<SmeGrowthEnablement/>} />
           <Route path="/about" element={<About/>} />
-          <Route path="/contact-us" element={<Contact/>} />
+          <Route path="/contact" element={<Contact/>} />
           
         </Routes>
         <PremiumFooter/>
